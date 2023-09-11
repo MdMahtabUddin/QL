2237b6b76f87bb05eafd8b62abaffbdb7384085b
MdMahtabUddin <mahtab.sentu@outlook.com>
2023-09-11 13:24:18 +0600

    access and interact with elements

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,4 +1,5 @@
 describe('template spec', () => {
+  
   it('passes', () => {
     cy.visit('https://example.cypress.io')
   })
--- cypress/e2e/test1.cy.js
+++ cypress/e2e/test1.cy.js
@@ -2,4 +2,11 @@
 
 it("Googl Search",()=>{
     cy.visit('http://google.com')
+
+    // add custom timeout 
+    cy.get('#APjFqb',{timeout:5000}).type('Welcome to Bangladesh{Enter}')
+   
+    // cy.contains(' Google Search ').click
+    cy.wait(2000)
+    // cy.contains(' Vedios ').click
 })
