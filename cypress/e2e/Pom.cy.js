f3f8f42287de7f7b5f8696702ac18f00b2e6c570
MdMahtabUddin <mahtab.sentu@outlook.com>
2023-09-12 15:44:14 +0600

    group test

--- cypress/e2e/Pom.cy.js
+++ cypress/e2e/Pom.cy.js
@@ -2,11 +2,25 @@ import { LoginPage } from "./pages/login_page"
 
 const loginPage =new LoginPage()
 
-it('page object model', () => {
+it('Login test 1', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com')
 
     loginPage.enterUsername()
     loginPage.unterPassward()
     loginPage.clickLogin()
+    
+    
 
   })
+
+  it('Login test 2', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com')
+
+    loginPage.enterUsername()
+    loginPage.unterPassward()
+    loginPage.clickLogin()
+    
+    
+
+  })
+
--- cypress/e2e/pages/login_page.js
+++ cypress/e2e/pages/login_page.js
@@ -1,17 +1,21 @@
 export class LoginPage{
 
+    username_textbox =':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input'
+    passward_textbox=':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input'
+    login_button='.oxd-button'
+    
     enterUsername(){
-        cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+        cy.get(this.username_textbox).type('Admin')
 
     }
 
     unterPassward(){
-        cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+        cy.get(this.passward_textbox).type('admin123')
 
     }
 
     clickLogin(){
-        cy.get('.oxd-button').click()
+        cy.get(this.login_button).click()
 
 
     }
